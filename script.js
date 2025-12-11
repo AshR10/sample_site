511a09dd1b0d72439368595bf9a2e5468058a7fc
AshR10 <ar10ashr@google.com>
2025-12-11 20:46:07 +0530

    adds final

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function changeColor() {
     document.body.style.backgroundColor = 'black';
 }
+let y=[]
+function calculate(){
+    let x;
+    if (document.getElementById('drop').value==='add')
+        x=Number(document.getElementById('i1').value)+Number(document.getElementById('i2').value);
+    else if (document.getElementById('drop').value==='substract')
+        x=Number(document.getElementById('i1').value)-Number(document.getElementById('i2').value);
+    else if (document.getElementById('drop').value==='multiply')
+        x=Number(document.getElementById('i1').value)*Number(document.getElementById('i2').value);
+    else if (document.getElementById('drop').value==='divide')
+        x=Number(document.getElementById('i1').value)/Number(document.getElementById('i2').value);
+    console.log(x);
+    document.getElementById('result').innerText = `${x}\n${y}`;
+    y.push(x);
+
+    
+}
